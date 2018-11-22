a4d37f8410907b0cd10aaa2c93fcbacf1e966c61
t-ishii <perlispython.plp@gmail.com>
2018-11-22 22:23:17 +0900

    Add effect when player get item

--- scenes/SpeedUpItem.gd
+++ scenes/SpeedUpItem.gd
@@ -1,6 +1,6 @@
-extends Sprite
+extends Node2D
 
 func _on_Area2D_body_entered(body):
     if visible and body.name == 'Player':
         body.emit_signal('get_item')
-        queue_free()
+        $Anime.play('get')
